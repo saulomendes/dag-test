c9c87ac0d2f6a90f5e2d575f536ff73af310983b
Saulo Mendes <saulo.alm@gmail.com>
2023-10-20 11:40:08 -0300

    new dag multdag

---
+++ dags/mult_dag.py
@@ -0,0 +1,19 @@
+from airflow.decorators import DAG
+from airflow.operators.empty import EmptyOperator
+
+dags = ['A', 'B', 'C']
+for dag_name in dags:
+    with DAG(
+                dag_id=f"mult_dag_{dag_name}",
+                catchup=False,
+                schedule=None,
+                tags=["mult_dag", dag_name]
+    ) as dag:
+        start = EmptyOperator(task_id="start")
+        end = EmptyOperator(task_id="end")
+        options = ["branch_a", "branch_b", "branch_c", "branch_d"]
+        for option in options:
+            t = EmptyOperator(task_id=option)
+            start >> t >> last
+
+
