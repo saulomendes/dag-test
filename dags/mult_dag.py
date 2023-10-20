459d9339545bb6f6370bf9d3220b5606ea29aaac
Saulo Mendes <saulo.alm@gmail.com>
2023-10-20 11:49:56 -0300

    fix import

--- dags/mult_dag.py
+++ dags/mult_dag.py
@@ -1,9 +1,12 @@
+import datetime
+
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
 
 dags = ['A', 'B', 'C']
 for dag_name in dags:
     with DAG(dag_id=f"mult_dag_{dag_name}",
+            start_date=datetime.datetime(2021, 1, 1)
             schedule="@daily",
             tags=["mult_dag", dag_name]
     ) as dag:
