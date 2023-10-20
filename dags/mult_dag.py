5f08f634356229af85c0426e07eebd93135de6d4
Saulo Mendes <saulo.alm@gmail.com>
2023-10-20 12:04:45 -0300

    add python operator

--- dags/mult_dag.py
+++ dags/mult_dag.py
@@ -1,7 +1,10 @@
 import datetime
+import time
 
 from airflow import DAG
 from airflow.operators.empty import EmptyOperator
+from airflow.operators.python import PythonOperator
+from random import randrange
 
 dags = ['A', 'B', 'C']
 for dag_name in dags:
@@ -14,7 +17,16 @@ for dag_name in dags:
         end = EmptyOperator(task_id="end")
         options = ["branch_a", "branch_b", "branch_c", "branch_d"]
         for option in options:
-            t = EmptyOperator(task_id=option)
+            # t = EmptyOperator(task_id=option)
+            python_task = PythonOperator(
+                task_id=options,
+                python_callable=lambda: time.sleep(randrange(10)),
+                # op_kwargs: Optional[Dict] = None,
+                # op_args: Optional[List] = None,
+                # templates_dict: Optional[Dict] = None
+                # templates_exts: Optional[List] = None
+            )    
+
             start >> t >> end
 
 
